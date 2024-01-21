6db6b647ee7933b6285766742117130b3103a122
DRausseo <145221251+DRausseo@users.noreply.github.com>
2024-01-20 22:40:41 -0300

    Finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -2,8 +2,17 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Solicitar el primer nombre
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Solicitar el apellido
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Mostrar el resultado
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
